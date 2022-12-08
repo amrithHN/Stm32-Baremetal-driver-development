a47a4aecf8183735a63921d390517f40fce7fc96
amrithHN <70368617+amrithHN@users.noreply.github.com>
2022-12-08 15:10:19 +0530

    added stm32f407xx.h
    added a CMSIS like header file with vectors

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/amrit/STM32CubeIDE/workspace_1.10.1/Stm32f4_BareMetalDrivers/drivers/inc/stm32f407xx.h
+C:/Users/amrit/STM32CubeIDE/workspace_1.10.1/Stm32f4_BareMetalDrivers/drivers/inc/stm32f407xx.h:
